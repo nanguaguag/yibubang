1e8b4fc971770e3f9d47b882a83833e9731c4b2c
nanguaguag <2022181640245@stu.scu.edu.cn>
2025-02-01 14:42:55 +0800

    适配android开发

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'screens/home_page.dart';
 import 'common/app_strings.dart';
 import 'package:get/get.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 设置系统UI样式，透明小白条背景
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.transparent, // 设置小白条背景颜色透明
+    systemNavigationBarIconBrightness: Brightness.light, // 图标颜色（亮色）
+    systemNavigationBarDividerColor: Colors.transparent, // 分割线颜色透明
+  ));
+
   runApp(const yibubang());
 }
 
